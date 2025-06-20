7008322f3e943b67150bec553d68714645da1863
Anakharsis9 <dayana530807@gmail.com>
2025-06-21 01:26:55 +0500

    feat: add formik

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -1,3 +1,24 @@
+import { Formik, Form, Field } from "formik";
+
 export const LoginPage = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={() => {
+          // TODO: handle me
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <Field type="text" name="username" />
+            <Field type="password" name="password" />
+            <button type="submit" disabled={isSubmitting}>
+              Войти
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 };
