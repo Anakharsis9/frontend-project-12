dd7a9c44438d7264715a90b2d58e48dbcc0c1334
Dayana Ismailova <dayana530807@gmail.com>
2025-06-24 12:56:09 +0500

    fix: update rollbar connection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,14 +4,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import { Provider } from 'react-redux'
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react'
 
 import { router } from './router'
 import { store } from './store'
 
+const rollbarConfig = {
+  accessToken: '6538f74707a14ee9ac00b7347619cef2',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  payload: {
+    environment: 'testenv',
+    // context: 'rollbar/test'
+    client: {
+      javascript: {
+        code_version: '1.0',
+        // source_map_enabled: true,
+        // guess_uncaught_frames: true
+      },
+    },
+  },
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   </StrictMode>,
 )
